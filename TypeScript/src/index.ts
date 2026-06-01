b8862e6cb5d9a377314b3dbc8ef79a8d677b5fe2
Sachanime <escalierquitue@gmail.com>
2026-06-01 22:59:28 +0200

    Init TypeScript Rework

---
+++ TypeScript/src/index.ts
@@ -0,0 +1,24 @@
+import { ActivityType, Client } from "discord.js"
+
+import ID from './ID-beta.json' with { type: 'json' }
+import Token from './token.json' with { type: 'json' }
+
+const client = new Client({ intents:[3276799] })
+
+client.login(Token.Beta)
+
+client.once('clientReady', async () => {
+
+    await client.application.fetch()
+    console.log(`Connected to ${client.application.name}`)
+    client.user.setPresence({ activities: [{ name: "Self development", type: ActivityType.Watching }] })
+
+})
+
+client.on('messageCreate', (message) => {
+
+    if(message.content == "!ping") {
+        message.reply(`Pong!`)
+    }
+
+})
