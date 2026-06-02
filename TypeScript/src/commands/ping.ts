0543f42110fc66d6923d2f99dcbe50c415aa8c72
Sachanime <escalierquitue@gmail.com>
2026-06-02 15:10:19 +0200

    Reduce ping.ts

--- TypeScript/src/commands/ping.ts
+++ TypeScript/src/commands/ping.ts
@@ -1,13 +1,5 @@
 import { ChatInputCommandInteraction } from 'discord.js'
 
 export function ping(interaction: ChatInputCommandInteraction) {
-
-    try {
         interaction.reply("Pong!")
-    }
-
-    catch(err) {
-        console.error('Reply error :\n', err)
-    }
-
 }
