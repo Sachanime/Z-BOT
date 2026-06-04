b37d76c9d868baff1fc61edc47690f80e3ab8eda
Sachanime <escalierquitue@gmail.com>
2026-06-04 22:53:40 +0200

    Export displayBanner

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,6 +1,4 @@
 import { Client, Events, ActivityType } from "discord.js"
-import path from 'path'
-import { displayBanner } from '../functions/displayBanner'
 
 export default {
 
@@ -8,12 +6,6 @@ export default {
 
     async execute(client: Client) {
 
-        const bannersPath = path.join(__dirname, '..', '..', 'assets', 'banners')
-        const editorBannerPath = path.join(bannersPath, 'editorBanner.txt')
-        const appBannerPath = path.join(bannersPath, 'appBanner.txt')
-
-        await displayBanner(editorBannerPath)
-        await displayBanner(appBannerPath)
         await client.application.fetch()
         console.log(`Connected to ${client.application.name}`)
         client.user.setPresence({ activities: [{ name: "Self development", type: ActivityType.Watching }] })
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Client } from 'discord.js'
+import path from 'path'
+import { displayBanner } from './functions'
+
+const bannersPath = path.join(__dirname, '..', '..', 'assets', 'banners')
+const editorBannerPath = path.join(bannersPath, 'editorBanner.txt')
+const appBannerPath = path.join(bannersPath, 'appBanner.txt')
+
+displayBanner(editorBannerPath)
+displayBanner(appBannerPath)
 
 import clientReady from './events/clientReady'
 import messageCreate from './events/messageCreate'
 import interactionCreate from './events/interactionCreate'
 import registerOnMessageCreate from './events/registerOnMessageCreate'
+import updateOnUserUpdate from './events/updateOnUserUpdate'
 
 const token = process.env.DISCORD_TOKEN
 
@@ -13,5 +23,6 @@ client.once(clientReady.name, () => clientReady.execute(client))
 client.on(messageCreate.name, (message) => messageCreate.execute(message))
 client.on(interactionCreate.name, (interaction) => interactionCreate.execute(interaction, client))
 client.on(registerOnMessageCreate.name, (message) => registerOnMessageCreate.execute(message))
+client.on(updateOnUserUpdate.name, (oldUser, newUser) => updateOnUserUpdate.execute(newUser))
 
 client.login(token)
