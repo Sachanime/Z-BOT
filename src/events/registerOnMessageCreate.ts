9a8abfff69def7629aeaba3789740b1cc4a563c0
Sachanime <escalierquitue@gmail.com>
2026-06-04 21:50:48 +0200

    Create registerOnMessageCreate event

---
+++ src/events/registerOnMessageCreate.ts
@@ -0,0 +1,27 @@
+import { Events, Message, MessageType } from 'discord.js'
+import { findUserWithId, createUser } from '../functions/database'
+import ID from '../ID.json'
+
+export default {
+
+    name: Events.MessageCreate,
+
+    async execute(message: Message) {
+
+        if(message.author.bot) { return }
+        if(message.type == MessageType.ChannelPinnedMessage) { return }
+            
+        const user = await findUserWithId(message.author.id)
+
+        if(user) { return }
+
+        else {
+
+            await createUser(message.author)
+            message.react(ID.Emotes.Registered)
+
+        }
+
+    }
+
+} as const
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { Client } from 'discord.js'
 
-import Token from '../src/token.json'
-
 import clientReady from './events/clientReady'
 import messageCreate from './events/messageCreate'
 import interactionCreate from './events/interactionCreate'
+import registerOnMessageCreate from './events/registerOnMessageCreate'
+
+const token = process.env.DISCORD_TOKEN
 
 const client = new Client({ intents:[3276799] })
 
 client.once(clientReady.name, () => clientReady.execute(client))
 client.on(messageCreate.name, (message) => messageCreate.execute(message))
 client.on(interactionCreate.name, (interaction) => interactionCreate.execute(interaction, client))
+client.on(registerOnMessageCreate.name, (message) => registerOnMessageCreate.execute(message))
 
-client.login(Token.Beta)
+client.login(token)
