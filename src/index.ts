8f9242e023f88e0370940461ce9b8c9578af14f7
Sachanime <escalierquitue@gmail.com>
2026-06-04 23:20:04 +0200

    Improve logs

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import { Client } from 'discord.js'
 import path from 'path'
 import { displayBanner } from './functions'
+import { config } from "dotenv"
+import { resolve } from "path"
 
-const bannersPath = path.join(__dirname, '..', '..', 'assets', 'banners')
+const bannersPath = path.join(__dirname, '..', 'assets', 'banners')
 const editorBannerPath = path.join(bannersPath, 'editorBanner.txt')
 const appBannerPath = path.join(bannersPath, 'appBanner.txt')
 
 displayBanner(editorBannerPath)
 displayBanner(appBannerPath)
 
+config({ path: resolve(__dirname, '../.env') })
+
 import clientReady from './events/clientReady'
 import messageCreate from './events/messageCreate'
 import interactionCreate from './events/interactionCreate'
 import registerOnMessageCreate from './events/registerOnMessageCreate'
 import updateOnUserUpdate from './events/updateOnUserUpdate'
+console.log("Events loaded")
 
 const token = process.env.DISCORD_TOKEN
 
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,9 +1,5 @@
-import { config } from "dotenv"
-import { resolve } from "path"
 import { PrismaMariaDb } from "@prisma/adapter-mariadb"
 import { PrismaClient } from './generated/prisma/client'
-
-config({ path: resolve(__dirname, '../.env') })
 
 const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
