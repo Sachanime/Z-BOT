3e828ea60f654385823ed0244071f803c392cc97
Sachanime <escalierquitue@gmail.com>
2026-06-06 15:05:53 +0200

    Add levelGoalEmbed

--- src/embeds/index.ts
+++ src/embeds/index.ts
@@ -2,5 +2,6 @@ import { createInfosEmbed } from './infosEmbed'
 import { createChangelogEmbed } from './changelogEmbed'
 import { createChangelogErrorEmbed } from './changelogErrorEmbed'
 import { createLevelUpEmbed } from './levelUpEmbed'
+import { createLevelGoalEmbed } from './levelGoalEmbed'
 
-export { createInfosEmbed, createChangelogEmbed, createChangelogErrorEmbed, createLevelUpEmbed }
+export { createInfosEmbed, createChangelogEmbed, createChangelogErrorEmbed, createLevelUpEmbed, createLevelGoalEmbed }
