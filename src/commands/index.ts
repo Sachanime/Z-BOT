94621fd920bddbaef6dc026467ff61797628f586
Sachanime <escalierquitue@gmail.com>
2026-06-10 22:05:11 +0200

    Move admin level subcommands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,22 +2,10 @@ import { executeInfosSlashCommand } from './infos'
 import { executeChangelogSlashCommand } from './changelog'
 import { executeLevelSlashCommand } from './level'
 import { executeSetupSlashCommand } from './setup'
-import { executeLevelGiveLevelSlashCommand } from './levelGiveLevel'
-import { executeLevelGiveXpSlashCommand } from './levelGiveXp'
-import { executeLevelRemoveLevelSlashCommand } from './levelRemoveLevel'
-import { executeLevelRemoveXpSlashCommand } from './levelRemoveXp'
-import { executeLevelSetLevelSlashCommand } from './levelSetLevel'
-import { executeLevelSetXpSlashCommand } from './levelSetXp'
 
 export {
     executeInfosSlashCommand,
     executeChangelogSlashCommand,
     executeLevelSlashCommand,
-    executeSetupSlashCommand,
-    executeLevelGiveLevelSlashCommand,
-    executeLevelGiveXpSlashCommand,
-    executeLevelRemoveLevelSlashCommand,
-    executeLevelRemoveXpSlashCommand,
-    executeLevelSetLevelSlashCommand,
-    executeLevelSetXpSlashCommand
+    executeSetupSlashCommand
 }
