eafb2b0a7fd5ba05bb72ea6c218f56b4945af805
Sachanime <escalierquitue@gmail.com>
2026-06-10 12:12:45 +0200

    Add admin level slash command builder

---
+++ src/commands/builders/adminLevelCommandBuilder.ts
@@ -0,0 +1,79 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+const adminLevelCommandBuilder = new SlashCommandBuilder()
+.setName('admin_level')
+.setDescription("Modifier le niveau ou les points XP d'un membre")
+.addSubcommand(subCommand => subCommand
+    .setName('give_level')
+    .setDescription("Donner des niveaux à un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription("Membre")
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription("Montant")
+    )
+)
+.addSubcommand(subCommand => subCommand
+    .setName('give_xp')
+    .setDescription("Donner des points XP à un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription("Membre")
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription("Montant")
+    )
+)
+.addSubcommand(subCommand => subCommand
+    .setName('remove_level')
+    .setDescription("Retirer des niveaux à un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription('Membre')
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription("Montant")
+    )
+)
+.addSubcommand(subCommand => subCommand
+    .setName('remove_xp')
+    .setDescription("Retirer des points XP à un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription("Membre")
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription('Montant')
+    )
+)
+.addSubcommand(subCommand => subCommand
+    .setName('set_level')
+    .setDescription("Définir le niveau d'un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription("Membre")
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription("Montant")
+    )
+)
+.addSubcommand(subCommand => subCommand
+    .setName('set_xp')
+    .setDescription("Définir le montant de points XP d'un membre")
+    .addUserOption(option => option
+        .setName('member')
+        .setDescription("Membre")
+    )
+    .addNumberOption(option => option
+        .setName('amount')
+        .setDescription("Montant")
+    )
+)
+
+export { adminLevelCommandBuilder }
